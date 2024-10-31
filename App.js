4aa0f85639784c6d5b2765793dcf819a2b7076d6
saikrishna <you@example.com>
2024-10-31 07:55:09 +0530

    parcel

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
-// /**
-// <div id="parent">
-//   <div id="child">
-//      <h1>H1 tag</h1>
-    //  <h2>h2 TAG</h2>
-//    </div>
-// </div>
-
-// */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent=React.createElement(
     "div",
@@ -15,19 +8,11 @@ const parent=React.createElement(
         React.createElement("div",
         {id:"child"},
         [
-        React.createElement("h1",{},"H1 tag"),
+        React.createElement("h1",{},"Helllo tag"),
         React.createElement("h2",{},"H2 tag")
         ]
     ),
 ]
 )
-
-
-
-
-
-
-// const heading=React.createElement("h1",{id:"heading" ,xyx:"abcd"}," Hello World From React !!");
-// console.log(heading);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
